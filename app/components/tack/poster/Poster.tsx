fa230b729d57d7db0489fabda063f683dbcdb4e6
DMRocks <sikora.samj@gmail.com>
2023-08-03 19:26:05 -0700

    Tack Board

---
+++ app/components/tack/poster/Poster.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+type PosterProps = {
+    
+
+
+
+    
+};
+
+const Poster:React.FC<PosterProps> = () => {
+    
+    return <div>
+
+
+
+
+    </div>
+}
+export default Poster;
