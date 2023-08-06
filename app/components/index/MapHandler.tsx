cdf6005a94a8f3c48aa94219516f1192d8ac8185
DMRocks <sikora.samj@gmail.com>
2023-08-06 14:54:39 -0700

    Added Note

--- app/components/index/MapHandler.tsx
+++ app/components/index/MapHandler.tsx
@@ -7,6 +7,8 @@ type MapHandlerProps = {
 
 const MapHandler:React.FC<MapHandlerProps> = () => {
 
+    //Temp***
+
     const [neighborhoodString, setNeighborhoodString] = React.useState('Queen Anne');
     
     return <div className='md:w-1/2 md:h-auto h-1/2 overflow-hidden'>
