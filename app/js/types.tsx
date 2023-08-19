1c187ff6487274ab64858cd4184cd1640d832601
DMRocks <sikora.samj@gmail.com>
2023-08-19 13:29:13 -0700

    Fleshing out Framework for Types

--- app/js/types.tsx
+++ app/js/types.tsx
@@ -1,48 +1,141 @@
-export interface Poster {
-    id: string;
+import { storage, firestore } from '@/app/js/firebase/firebase';
+import { FirebaseStorage } from 'firebase/storage';
+import { Firestore, collection, getDoc, getDocs } from 'firebase/firestore';
+
+class Poster {
+
+    city : string; //must be one of "seattle" or "kelowna"
+    owner : string;
     title: string;
     description: string;
+    id: string;
+    neighborhood : string[];
     imageRef : string;
-    location : Telepole[];
-    owner : string;
+    keywords : string[];
     created : Date;
+    expiration : Date;
     reccuring : boolean;
     reccuringDays : number[];
-    keywords : string[];
-    neighborhood : string;
+    firebaseRef : Firestore;
+
+
+
+    constructor(city: string, id?: string, owner?: string,  title?: string, description?: string, neighborhood?: string[], imageRef?: string, keywords?: string[], created?: Date, expiration?: Date, reccuring?: boolean, reccuringDays?: number[]) {
+
+        this.firebaseRef = firestore;
+
+        // You can either have the id and nothing else, or you can only leave out the id.
+        if (id) {
+            //TODO: Get the poster from the database.
+            const collectionRef = collection(this.firebaseRef, `cities/${city}/posters`);
+
+            getDocs(collectionRef)
+                .then((snapshot) => {
+                    snapshot.docs.forEach((doc) => {
+                        if(doc.id == id) {
+                            this.city = city;
+                            this.id = doc.id;
+
+                            this.owner = doc.data().owner;
+                            this.title = doc.data().title;
+                            this.description = doc.data().description;
+                            this.neighborhood = doc.data().neighborhood;
+                            this.imageRef = doc.data().imageRef;
+                            this.keywords = doc.data().keywords;
+                            this.created = doc.data().created;
+                            this.expiration = doc.data().expiration;
+                            this.reccuring = doc.data().reccuring;
+                            this.reccuringDays = doc.data().reccuringDays;
+                            return;
+                        }
+                    })
+                    throw new Error('Poster not found.');  
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                })
+        }
+        
+        if(!owner || !title || !description || !neighborhood || !imageRef || !keywords || !created || !expiration || !reccuring || !reccuringDays) {
+            throw new Error('Missing required parameters.');
+        }
+
+        this.owner = owner;
+        this.city = city;
+        this.title = title;
+        this.description = description;
+        this.neighborhood = neighborhood;
+        this.imageRef = imageRef;
+        this.keywords = keywords;
+        this.created = created;
+        this.expiration = expiration;
+        this.reccuring = reccuring;
+        this.reccuringDays = reccuringDays;
+
+        this.id = "TEMP";
+
+
+    }
+
 }
 
-export interface Sticker {
+class Sticker {
+
     id: string;
     title: string;
     imageRef: string;
     httpRef : string;
-}
 
-export interface Neighborhood {
-    name: string;
-    city: string;
-    location : {
-        lat: number;
-        lng: number;
+    constructor(id: string, title: string, imageRef: string, httpRef : string) {
+        this.id = id;
+        this.title = title;
+        this.imageRef = imageRef;
+        this.httpRef = httpRef;
     }
 
-    telepoles : Telepole[];
 }
 
-export interface Telepole {
+//export interface Neighborhood {
+//    name: string;
+//    city: string;
+//    location : {
+//        lat: number;
+//        lng: number;
+//    }
+//}
+
+class Pole {
+
     id: number;
-    neighborhood: string;
     location : {
         lat: number;
         lng: number;
     }
     postersID: string[];
     stickersID: string[];
+    fireBaseStorageRef : FirebaseStorage;
+
+    constructor(id: number, location : {lat: number, lng: number}, postersID: string[], stickersID: string[]) {
+
+        this.fireBaseStorageRef = storage;
+
+        this.id = id;
+        this.location = location;
+        this.postersID = postersID;
+        this.stickersID = stickersID;
+    }
+
 }
 
-export interface User {
+class User {
     name: string;
     email: string;
     mainNeighborhood : string;
+
+    constructor(name: string, email: string, mainNeighborhood : string) {
+        this.name = name;
+        this.email = email;
+        this.mainNeighborhood = mainNeighborhood;
+    }
+
 }
