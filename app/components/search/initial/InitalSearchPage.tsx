5c7ac742b9499b8f0a57fe84c323d8e45aa52af1
Sam <sikora.samj@gmail.com>
2023-09-25 10:06:16 -0700

    Intial Search Page work

--- app/components/search/initial/InitalSearchPage.tsx
+++ app/components/search/initial/InitalSearchPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import SearchBar from './SearchBar';
 import SearchBox from './SearchBox';
 
@@ -10,13 +10,25 @@ const InitalSearchPage:React.FC<InitalSearchPageProps> = () => {
 
 
     const [searchbyEvents, setSearchbyEvents] = React.useState(true);
+
+    const neighborhoodButtonPress:MouseEventHandler<HTMLButtonElement> = (event) => {
+        setSearchbyEvents(false);
+    }
+
+    const eventButtonPress:MouseEventHandler<HTMLButtonElement> = (event) => {
+        setSearchbyEvents(true);
+    }
     
     return <div>
         
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
         <SearchBar searchableUnits={["kdf"]} />
-        <SearchBox iconName='pin_drop' name='Search' />
+
+        <div>
+            <SearchBox iconName='pin_drop' name='Neighborhood' selected = {!searchbyEvents} onClick={neighborhoodButtonPress} />
+            <SearchBox iconName='groups' name='Event' selected = {searchbyEvents} onClick={eventButtonPress} />
+        </div>
 
 
 
--- app/components/search/initial/SearchBox.tsx
+++ app/components/search/initial/SearchBox.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 
 type SearchBoxProps = {
     iconName: string;
     name: string;
+    selected: boolean;
+    onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-const SearchBox:React.FC<SearchBoxProps> = ({iconName, name}) => {
+const SearchBox:React.FC<SearchBoxProps> = ({iconName, name, selected, onClick}) => {
+
+    // selected #094d12
+    // unslected #35AA44
     
-    return <div className='w-full h-auto'>
+    return <button onClick={onClick} className='w-full h-auto'>
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <span className="material-symbols-outlined bg-spgreen p-5 text-lg">{iconName}</span>
+
+        <span className={"material-symbols-outlined p-5 rounded-full" + (selected ? " bg-[#094d12]" : " bg-[#77e689]")} style={{fontSize: '3rem'}}>{iconName}</span>
+
+        <div>
+            Search by {name}
+        </div>
         
-    </div>
+    </button>
 }
 export default SearchBox;
