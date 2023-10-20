16d00d186bbbc788e1acf51a1f3a2d6ac1a8b6ca
Sam <sikora.samj@gmail.com>
2023-10-20 10:16:51 -0700

    Map Fall Back

--- app/components/index/MapHandler.tsx
+++ app/components/index/MapHandler.tsx
@@ -1,12 +1,18 @@
 "use client";
 import React from 'react';
+import Image from 'next/image';
 import MainMap from './MainMap';
+import StaticMap from '../../../public/assets/map_fallback.png'
 
 type MapHandlerProps = {
     neighborhood: string
 };
 
 const MapHandler:React.FC<MapHandlerProps> = ({neighborhood}) => {
+
+    if(neighborhood === '') return <div className='md:w-1/2 md:h-auto h-1/2 overflow-hidden'>
+        <Image src={StaticMap} alt={"A map to be blurred"}/>
+    </div>
 
     return <div className='md:w-1/2 md:h-auto h-1/2 overflow-hidden'>
         <MainMap neighborhoodString={neighborhood}/>
