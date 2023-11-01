1d71cc5673ba70b51b89de3438afdde6128aa87a
ataschuk <alextaschuk@gmail.com>
2023-11-01 12:35:57 -0700

    Expanded coordinates for neighborhoods

--- app/components/index/MainMap.tsx
+++ app/components/index/MainMap.tsx
@@ -10,6 +10,9 @@ type MainMapProps = {
 
 const MainMap: React.FC<MainMapProps> = ({ neighborhoodString }) => {
 
+  console.log(neighborhoodString);
+  console.log(mapboxNeighborhood[neighborhoodString]);
+
   return <div className='w-full h-full'>
     <Map
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
