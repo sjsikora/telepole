bae6c069e9cc15ae8257bb033a165ba0b8498253
Sam <sikora.samj@gmail.com>
2024-01-25 10:55:41 -0800

    Visually clearer Buttons

--- app/components/search/initial/SearchBox.tsx
+++ app/components/search/initial/SearchBox.tsx
@@ -11,14 +11,24 @@ const SearchBox:React.FC<SearchBoxProps> = ({iconName, name, selected, onClick})
 
     // selected #094d12
     // unslected #35AA44
+
+    const styleSelected = {
+        fontSize: '3rem',
+        color: "black",
+    }
+
+    const styleUnselected = {
+        fontSize: '3rem',
+        color: "gray",
+    }
     
     return <button onClick={onClick} className='max-w-1/3'>
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
-        <span className={"material-symbols-outlined p-5 rounded-full fill-white " + (selected ? "bg-[#39ba4b]" : "border-2 border-stone-100")} style={{fontSize: '3rem'}}>{iconName}</span>
+        <span className={"material-symbols-outlined p-5 drop-shadow-2xl rounded-2xl fill-white " + (selected ? "bg-[#39ba4b]" : "border-2 border-stone-100")} style={(selected ? styleSelected : styleUnselected)}>{iconName}</span>
 
-        <div className={selected ? "" : "text-gray-500"}>
+        <div className={selected ? "" : "text-gray-10i"}>
             Search by {name}
         </div>
         
