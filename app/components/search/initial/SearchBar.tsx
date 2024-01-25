b33b7b857126c1d47edf51e06f477c3d4699cca8
Sam <sikora.samj@gmail.com>
2024-01-25 10:55:59 -0800

    Work on Search Page Search Bar

--- app/components/search/initial/SearchBar.tsx
+++ app/components/search/initial/SearchBar.tsx
@@ -29,19 +29,21 @@ const SearchBar: React.FC<SearchBarProps> = ({ city, searchableUnits }) => {
 
     return <div className='rounded-xl border-2 border-black p-2'>
 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
         <form onSubmit={handleFormSubmit}>
 
-            <label htmlFor="search-keyword">
-                <span className="visually-hidden"></span>
+            <label className='content-end' htmlFor="search-keyword">
+                <span className="material-symbols-outlined visually-hidden"></span>
             </label>
 
             <select name="key" id="search-keyword">
-                {Object.keys(searchableUnits).map((key) => {
+                {Object.keys(searchableUnits).map((key) => {    
                     return <option key={key} value={key}>{searchableUnits[key]}</option>
                 })}
             </select>
 
-            <button className='3' type='submit'> Search </button>
+            <button type='submit'> Search </button>
         </form>
 
 
